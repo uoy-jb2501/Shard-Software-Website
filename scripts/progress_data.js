6556fc4ea985196e0fbed6f55d3d4b6ac7344876
uoy-jb2501 <94061765+uoy-jb2501@users.noreply.github.com>
2022-01-26 18:31:22 +0000

    Added plan for 2022-01-22

--- scripts/progress_data.js
+++ scripts/progress_data.js
@@ -3,6 +3,65 @@ function daysToMilliseconds(days) {
 }
 
 progressData = {
+
+    "2022-01-22":[
+        ['ReviewExisting', 'Review Existing Systems', "Requirements",
+        new Date(2021, 10, 26), null, daysToMilliseconds(7),  100,  null],
+
+        ['Interview1', 'Conduct Interview', "Requirements",
+        new Date(2021, 10, 29), null, daysToMilliseconds(1),  100,  ""],
+
+        ['WriteUpRequirements', 'Write Up Requirements', "Requirements",
+        null, null, daysToMilliseconds(7),  30,  "Interview1"],
+
+        ['Design', 'Design', "Design",
+        new Date(2021, 11, 2), null, daysToMilliseconds(15),  100,  "Interview1,ReviewExisting"],
+
+        ['libgdx', 'Setup libGDX', "Implementation",
+        new Date(2021, 11, 2), null, daysToMilliseconds(2),  100,  "Interview1"],
+
+        ['intro', 'Setup Splashscreen', "Implementation",
+        null, null, daysToMilliseconds(4),  100,  "libgdx"],
+
+        ['resClass', 'Setup ResourceUtil Class', "Implementation",
+        new Date(2021, 11, 11), null, daysToMilliseconds(4),  100,  "libgdx"],
+
+        ['entClass', 'Setup Entity Class', "Implementation",
+        new Date(2022, 00, 06), null, daysToMilliseconds(3), 90,  "resClass"],
+
+        ['shipClass', 'Setup Ship Class', "Implementation",
+        null, null, daysToMilliseconds(4),  90,  "entClass"],
+
+        ['control', 'Setup Control System', "Implementation",
+        null, null, daysToMilliseconds(2),  100,  "shipClass"],
+
+        ['debug', 'Setup Debug System', "Implementation",
+        new Date(2022, 00, 17), null, daysToMilliseconds(2),  100,  "libgdx"],
+
+        ['worldClass', 'Setup World Class', "Implementation",
+        new Date(2022, 00, 13), null, daysToMilliseconds(9),  100,  "libgdx,resClass"],
+
+        ['minimap', 'Setup Minimap', "Implementation",
+        null, null, daysToMilliseconds(3),  100,  "worldClass,resClass"],
+
+        ['collisions', 'Setup World-Ent Collisions', "Implementation",
+        null, null, daysToMilliseconds(2),  100,  "worldClass,entClass"],
+
+        ['enemyAI', 'Setup AI-Controlled Ships', "Implementation",
+        null, null, daysToMilliseconds(5),  0,  "worldClass,shipClass"],
+
+        ['collegeClass', 'Setup College Classes', "Implementation",
+        new Date(2022, 00, 06), null, daysToMilliseconds(2),  0,  "resClass"],
+
+        ['collegePlacement', 'Setup College Placement', "Implementation",
+        null, null, daysToMilliseconds(1),  0,  "collegeClass,worldClass"],
+
+        ['collegeCombat', 'Setup College (non-ship) Combat', "Implementation",
+        null, null, daysToMilliseconds(5),  0,  "collegePlacement"],
+
+        ['cannonballs', 'Setup Cannonball Firing System', "Implementation",
+        null, null, daysToMilliseconds(2),  80,  "shipClass"],
+    ],
     
     "2022-01-14":[
         ['ReviewExisting', 'Review Existing Systems', "Requirements",
@@ -15,7 +74,7 @@ progressData = {
         null, null, daysToMilliseconds(7),  30,  "Interview1"],
 
         ['Design', 'Design', "Design",
-        new Date(2021, 11, 2), null, daysToMilliseconds(25),  100,  "Interview1,ReviewExisting"],
+        new Date(2021, 11, 2), null, daysToMilliseconds(15),  100,  "Interview1,ReviewExisting"],
 
         ['libgdx', 'Setup libGDX', "Implementation",
         new Date(2021, 11, 2), null, daysToMilliseconds(2),  100,  "Interview1"],
