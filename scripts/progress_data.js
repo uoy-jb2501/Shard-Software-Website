f4a79a4adbb7e9f64c2088cce41302a9ab6cb133
uoy-jb2501 <94061765+uoy-jb2501@users.noreply.github.com>
2022-02-01 23:53:15 +0000

    Added latest plan

--- scripts/progress_data.js
+++ scripts/progress_data.js
@@ -4,6 +4,68 @@ function daysToMilliseconds(days) {
 
 progressData = {
 
+    "2022-02-01":[
+        ['ReviewExisting', 'Review Existing Systems', "Requirements",
+        new Date(2021, 10, 26), null, daysToMilliseconds(7),  100,  null],
+
+        ['Interview1', 'Conduct Interview', "Requirements",
+        new Date(2021, 10, 29), null, daysToMilliseconds(1),  100,  ""],
+
+        ['WriteUpRequirements', 'Write Up Requirements', "Requirements",
+        null, null, daysToMilliseconds(7),  100,  "Interview1"],
+
+        ['Design', 'Design', "Design",
+        new Date(2021, 11, 2), null, daysToMilliseconds(15),  100,  "Interview1,ReviewExisting"],
+
+        ['libgdx', 'Setup libGDX', "Implementation",
+        new Date(2021, 11, 2), null, daysToMilliseconds(2),  100,  "Interview1"],
+
+        ['intro', 'Setup Splashscreen', "Implementation",
+        null, null, daysToMilliseconds(4),  100,  "libgdx"],
+
+        ['resClass', 'Setup ResourceUtil Class', "Implementation",
+        new Date(2021, 11, 11), null, daysToMilliseconds(4),  100,  "libgdx"],
+
+        ['entClass', 'Setup Entity Class', "Implementation",
+        new Date(2022, 00, 06), null, daysToMilliseconds(3), 100,  "resClass"],
+
+        ['shipClass', 'Setup Ship Class', "Implementation",
+        null, null, daysToMilliseconds(4),  100,  "entClass"],
+
+        ['control', 'Setup Control System', "Implementation",
+        null, null, daysToMilliseconds(2),  100,  "shipClass"],
+
+        ['debug', 'Setup Debug System', "Implementation",
+        new Date(2022, 00, 06), null, daysToMilliseconds(2),  100,  "libgdx"],
+
+        ['worldClass', 'Setup World Class', "Implementation",
+        new Date(2022, 00, 10), null, daysToMilliseconds(9),  100,  "libgdx,resClass"],
+
+        ['minimap', 'Setup Minimap', "Implementation",
+        null, null, daysToMilliseconds(3),  100,  "worldClass,resClass"],
+
+        ['collisions', 'Setup World-Ent Collisions', "Implementation",
+        null, null, daysToMilliseconds(2),  100,  "worldClass,entClass"],
+
+        ['enemyAI', 'Setup AI-Controlled Ships', "Implementation",
+        null, null, daysToMilliseconds(5),  100,  "worldClass,shipClass"],
+
+        ['collegeClass', 'Setup College Classes', "Implementation",
+        null, null, daysToMilliseconds(2),  100,  "entClass"],
+
+        ['collegePlacement', 'Setup College Placement', "Implementation",
+        null, null, daysToMilliseconds(1),  100,  "collegeClass,worldClass"],
+
+        ['collegeCombat', 'Setup College (non-ship) Combat', "Implementation",
+        null, null, daysToMilliseconds(5),  100,  "collegePlacement,cannonballs"],
+
+        ['cannonballs', 'Setup Cannonball Firing System', "Implementation",
+        null, null, daysToMilliseconds(2),  100,  "shipClass"],
+
+        ['Deploy', 'Release Game', "Deploy",
+		null, null, daysToMilliseconds(1), 100, "collegeCombat,enemyAI"]
+    ],
+
     "2022-01-22":[
         ['ReviewExisting', 'Review Existing Systems', "Requirements",
         new Date(2021, 10, 26), null, daysToMilliseconds(7),  100,  null],
@@ -62,7 +124,7 @@ progressData = {
         ['cannonballs', 'Setup Cannonball Firing System', "Implementation",
         null, null, daysToMilliseconds(2),  80,  "shipClass"],
 
-        ['Submit', 'Submit Project', "Deploy",
+        ['Deploy', 'Release Game', "Deploy",
 		null, null, daysToMilliseconds(1), 0, "collegeCombat,enemyAI"]
     ],
     
@@ -124,7 +186,7 @@ progressData = {
         ['cannonballs', 'Setup Cannonball Firing System', "Implementation",
         null, null, daysToMilliseconds(2),  0,  "shipClass"],
 
-        ['Submit', 'Submit Project', "Deploy",
+        ['Deploy', 'Release Game', "Deploy",
 		null, null, daysToMilliseconds(1), 0, "collegeCombat,enemyAI"]
     ],
 
@@ -155,6 +217,9 @@ progressData = {
 
         ['ImplementFront', 'Implement Frontend', "Implementation",
         null, null, daysToMilliseconds(7*4),  1,  "Prototype"],
+
+        ['Deploy', 'Release Game', "Deploy",
+		null, null, daysToMilliseconds(1), 0, "ImplementBack,ImplementFront"]
     ]
 
 
